7b5afff012f52283e217bcd4acd70ffe6074c773
AntonShiva <ryashinanthon@yandex.ru>
2024-11-15 15:36:56 +0300

    Initial Commit

---
+++ Melodic Music Player/Melodic_Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Melodic_Music_PlayerApp.swift
+//  Melodic Music Player
+//
+//  Created by Anton Rasen on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Melodic_Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
