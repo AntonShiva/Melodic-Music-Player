be6e0b1ca41c7c7b1efe2fa0c8d113a3cd8125da
AntonShiva <ryashinanthon@yandex.ru>
2024-11-24 20:47:36 +0300

    Добавил возможност с реального телефона открывать файлы при помощи AccessingSecurityScopedResource

--- Melodic Music Player/Utilities/FileManager.swift
+++ Melodic Music Player/Utilities/FileManager.swift
@@ -27,39 +27,42 @@ struct FileManager: View {
                 do {
                     let urls = try result.get()
                     guard let url = urls.first else { return }
-                    
-                    let melodyData = try Data(contentsOf: url)
-                    let melodyAsset = AVURLAsset(url: url)
-                    var melody = Model(name: url.lastPathComponent, data: melodyData)
-                    
-                    // Загружаем метаданные
-                    let metadata = try await melodyAsset.load(.metadata)
-                    for item in metadata {
-                        guard let key = item.commonKey?.rawValue else { continue }
+                    // добавил возможност с реального телефона открывать файлы при помощи AccessingSecurityScopedResource
+                    if url.startAccessingSecurityScopedResource(){
+                        let melodyData = try Data(contentsOf: url)
+                        let melodyAsset = AVURLAsset(url: url)
+                        var melody = Model(name: url.lastPathComponent, data: melodyData)
                         
-                        switch key {
-                        case AVMetadataKey.commonKeyArtist.rawValue:
-                            melody.artist = try await item.load(.value) as? String
-                        case AVMetadataKey.commonKeyArtwork.rawValue:
-                            melody.image = try await item.load(.value) as? Data
-                        case AVMetadataKey.commonKeyTitle.rawValue:
-                            if let title = try await item.load(.value) as? String {
-                                melody.name = title
+                        // Загружаем метаданные
+                        let metadata = try await melodyAsset.load(.metadata)
+                        for item in metadata {
+                            guard let key = item.commonKey?.rawValue else { continue }
+                            
+                            switch key {
+                            case AVMetadataKey.commonKeyArtist.rawValue:
+                                melody.artist = try await item.load(.value) as? String
+                            case AVMetadataKey.commonKeyArtwork.rawValue:
+                                melody.image = try await item.load(.value) as? Data
+                            case AVMetadataKey.commonKeyTitle.rawValue:
+                                if let title = try await item.load(.value) as? String {
+                                    melody.name = title
+                                }
+                            default:
+                                break
                             }
-                        default:
-                            break
                         }
-                    }
-                    
-                    // Загружаем длительность
-                    let duration = try await melodyAsset.load(.duration)
-                    melody.duration = CMTimeGetSeconds(duration)
-                    
-                    // Добавляем мелодию, если её нет в списке
-                    if !self.melodies.contains(where: { $0.name == melody.name }) {
-                        DispatchQueue.main.async {
-                            self.melodies.append(melody)
+                        
+                        // Загружаем длительность
+                        let duration = try await melodyAsset.load(.duration)
+                        melody.duration = CMTimeGetSeconds(duration)
+                        
+                        // Добавляем мелодию, если её нет в списке
+                        if !self.melodies.contains(where: { $0.name == melody.name }) {
+                            DispatchQueue.main.async {
+                                self.melodies.append(melody)
+                            }
                         }
+                        url.stopAccessingSecurityScopedResource()
                     }
                 } catch {
                     print("Ошибка чтения файла: \(error.localizedDescription)")
